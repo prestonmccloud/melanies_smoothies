727a28a99278c3b622e0fbd098e4d83505a1a7b9
prestonmccloud <prestonmccloud@icloud.com>
2025-07-07 17:28:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,9 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ''
+
+    search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+    st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
